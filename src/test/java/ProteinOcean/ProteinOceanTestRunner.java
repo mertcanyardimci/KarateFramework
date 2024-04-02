ac7d2e071d3adf64adb9a4327a715eca4dcebe79
Mertcan Yardımcı <mertcanyardimci97@gmail.com>
2024-04-02 17:27:03 +0300

    update

--- src/test/java/ProteinOcean/ProteinOceanTestRunner.java
+++ src/test/java/ProteinOcean/ProteinOceanTestRunner.java
@@ -2,9 +2,11 @@ package ProteinOcean;
 
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
+import org.junit.jupiter.api.Test;
 
 public class ProteinOceanTestRunner {
-    public static void main(String[] args) {
+    @Test
+    public void main() {
         Results results = Runner.path("classpath:ProteinOcean/ProteinOcean.feature").tags("~@page").parallel(1);
         String reportDir = results.getReportDir();
         System.out.println("Test raporu kaydedildi: " + reportDir);
---
+++ src/test/java/ProteinOcean/karate-config.js
@@ -0,0 +1,10 @@
+function fn() {
+  var config = {
+    karate: {
+      feature: 'classpath:java/ProteinOcean/ProteinOcean.feature'
+    }
+  };
+  return config;
+}
+
+//Run Command:mvn test -Dtest=ProteinOceanTestRunner
